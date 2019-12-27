1d42a1f2d7f3bc15a83d44e275c740d01d7b27bc
MahmoudHammad <hodaman2012@gmail.com>
2019-12-27 05:47:07 +0200

    make rev page work perfectly

--- rev/script.js
+++ rev/script.js
@@ -1,15 +1,21 @@
 const data = [
-    {label:"heat tran1", value: { rec: "https://drive.google.com/file/d/1VRpK-ywyTwQGZBfZ7cIeN1vXHU57-cKo/preview", pdf: "https://drive.google.com/file/d/1Tt-xoeqQitA3ox6tOYbIoHcyn6F61gwD/preview" } },
-    {label:"engines", value: { rec: "", pdf: "" } },
-    {label:"thermo", value: { rec: "", pdf: "" } }
+    {lable:"transfer 1",label:"heat transfer 1", value: { rec: "https://drive.google.com/file/d/1HQNOG0fyPR1_I6P2M8QE0Exzmpk2ULnz/preview", pdf: "https://drive.google.com/file/d/12WVBb7Z8zZGIWDxC0cpPaoFD3X4YJh-q/preview" } },
+    {lable:"transfer 2",label:"heat transfer2", value: { rec: "https://drive.google.com/file/d/1VRpK-ywyTwQGZBfZ7cIeN1vXHU57-cKo/preview", pdf: "https://drive.google.com/file/d/1Tt-xoeqQitA3ox6tOYbIoHcyn6F61gwD/preview" } },
+    {lable:"engines",label:"engines + Final Exams", value: { rec: "https://drive.google.com/file/d/1okiSimFeYe7cGdXWOaLpnpi-_61mBOdj/preview", pdf: "https://drive.google.com/file/d/1f2BlsCAF9QCAH6z6N9qtsjwE0CK6Zdqi/preview" } },
+    {lable:"thermo 1",label:"thermodynamics before Mid Term", value: { rec: "https://drive.google.com/file/d/1-A7mUB8sCja1-vBX74VRuzS8Y82u8Dao/preview", pdf: "https://drive.google.com/file/d/1ewP-uuxdPjK1UnSyhDM4XTkzdbzXpyx7/preview" } }
+   , {lable:"thermo 2",label:"thermodynamics after Mid Term", value: { rec: "https://drive.google.com/file/d/14gw6N-nenYMfIa9BPIiRerwLoCuaF8OO/preview", pdf: "https://drive.google.com/file/d/1IeEYvpW-ruuY9wQeP7H49AmKkVK4lR1b/preview" } }
 ]
-let content = document.querySelector('.content')
+const content = document.querySelector('.content')
+const btns = document.querySelector('.btns')
 
 
 function renderbtn(){
-    console.log(data[0].value)
-    let btn=createbtn("HeatTransFerr1","btn",data[0].label ,data[0].value)
-    content.appendChild(btn)
+
+    for (const e of data) {
+        console.log(e)    
+        let btn=createbtn(e.lable,"btn",e.label ,e.value)
+        btns.appendChild(btn)
+    }
 }
 // setTimeout(renderbtn,500 )
 renderbtn()
@@ -25,6 +31,7 @@ function createbtn(lable, classe ,label,value) {
 }
 
 function create(label, recsrc, pdfsrc) {
+
     let rev = document.createElement("div")
     rev.className = "rev"
 
@@ -42,19 +49,25 @@ function create(label, recsrc, pdfsrc) {
     rev.appendChild(hd3)
     rev.appendChild(rec)
     rev.appendChild(pdf)
+
     render(rev)
 
     return rev
 }
 
-// document.replaceChild(new, old)
 
+let c=0;
+let old=0;
 function render(child) {
-    // let btn = createbtn("Heat Rev1 ", "btn btn-danger")
-    // content.appendChild(btn)
-    // let rev = create("heat transfer 1", "https://drive.google.com/file/d/1VRpK-ywyTwQGZBfZ7cIeN1vXHU57-cKo/preview", "https://drive.google.com/file/d/1Tt-xoeqQitA3ox6tOYbIoHcyn6F61gwD/preview")
-    content.appendChild(child)
-
+    if(c===0){
+        content.appendChild(child)
+        old=child
+    }
+    else{
+        if(child!==old){
+            content.replaceChild(child, old)
+            old=child
+        }
+    }
+    c++
 }
-
-console.log(content)
