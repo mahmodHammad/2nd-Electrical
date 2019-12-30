e4c91fe587824a9cd027e62e09db311d6365fabc
Mahmoud Hammad <hodaman2012@gmail.com>
2019-12-30 17:38:01 +0200

    shitty bug occures

--- scripts/createbtns.js
+++ scripts/createbtns.js
@@ -5,35 +5,102 @@ export default class createIframe{
       this.data=data
       this.name="Hammad"
     }
-    //  renderbtn = (givenData, container = content) => {
-    //   let btns = document.createElement("div");
-    //   btns.className = "btns";
-    //   container.appendChild(btns);
+     renderbtn = (givenData, container = content) => {
+      let btns = document.createElement("div");
+      btns.className = "btns";
+      container.appendChild(btns);
     
-    //   for (const e of givenData) {
-    //     let btn = createbtn(e.title, "btn", e.label, e.value, container);
-    //     btns.appendChild(btn);
-    //   }
-    // };
-    //  createbtn(title, classe, label, value, container) {
-    //   let btn = document.createElement("button");
-    //   btn.className = classe;
-    //   btn.innerText = title;
-    //   btn.onclick = () =>
-    //     createIframe(label, value.rec, value.pdf, value.video, container);
-    //   return btn;
-    // }
-
-    sayHello(){
-        console.log("hello mother fucker")
+      for (const e of givenData) {
+        let btn = createbtn(e.title, "btn", e.label, e.value, container);
+        btns.appendChild(btn);
+      }
+    };
+     createbtn(title, classe, label, value, container) {
+      let btn = document.createElement("button");
+      btn.className = classe;
+      btn.innerText = title;
+      btn.onclick = () =>
+        createIframe(label, value.rec, value.pdf, value.video, container);
+      return btn;
     }
 
-    sayby(){
-        console.log(this.data)
-        console.log(this.name)
+
+    
+function createIframe(label, recsrc, pdfsrc, video, container) {
+    let scope = document.createElement("div");
+    scope.className = "rev";
+    
+    let hd3 = document.createElement("h3");
+    hd3.innerText = label;
+    scope.appendChild(hd3);
+  
+    if (recsrc) {
+      let rec = document.createElement("iframe");
+      rec.src = recsrc;
+      rec.className = "record";
+      scope.appendChild(rec);
     }
-    saythis(){
-        console.log(this)
+    if (pdfsrc) {
+      let scroll = document.createElement("a");
+      scroll.className = "scroll";
+      scroll.href = "#doc";
+      scroll.innerHTML = "scroll to pdf";
+      scope.appendChild(scroll);
+  
+      let pdf = document.createElement("iframe");
+      pdf.src = pdfsrc;
+      pdf.className = "pdf";
+      pdf.id = "doc";
+      scope.appendChild(pdf);
     }
-   }
+  
+    if (video) {
+      let videoContainer = document.createElement("div");
+      videoContainer.className = "subject-video";
+  
+      if (video.h1) {
+        let videoHeader = document.createElement("h1");
+        videoHeader.innerText = video.h1;
+        videoContainer.appendChild(videoHeader);
+      }
+  
+      if (video.p) {
+        let videoparagraph = document.createElement("p");
+        videoparagraph.innerText = video.p;
+        videoContainer.appendChild(videoparagraph);
+      }
+  
+      let videoIframe = document.createElement("iframe");
+      videoIframe.src = video.src;
+  
+      videoIframe.frameborder = "0";
+      videoIframe.allow =
+        "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+      videoIframe.allowfullscreen = true;
+      
+      videoContainer.appendChild(videoIframe)
+      scope.appendChild(videoContainer);
+    }
+  
+    render(scope, container);
+  
+    return scope;
+  }
+  
+  let c = 0;
+  let old = 0;
+  function render(child, container) {
+    if (c === 0) {
+      container.appendChild(child);
+      old = child;
+      // footer.style.display = "block";
+    } else {
+      if (child !== old) {
+        container.replaceChild(child, old);
+        old = child;
+      }
+    }
+    c++;
+  }
+
   
