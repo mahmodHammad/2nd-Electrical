18fad487d42c412e0eca9afe568589acf5ff7001
Mahmoud Hammad <hodaman2012@gmail.com>
2020-01-02 04:17:32 +0200

    -

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-// self.addEventListener('install', function (event) {
-//   console.log('SW Installed');
-//   event.waitUntil(
-//     caches.open('static')
-//       .then(function (cache) {
-//         // cache.add('/');
-//         // cache.add('/index.html');
-//         // cache.add('/src/js/app.js');
-//         cache.addAll([
-//           '/',
-//           '/index.html',
-//           '/src/js/app.js',
-//           '/src/js/index.js',
-//           '/src/js/subjects/field.js',
-//           '/src/css/style.css',
-//           '/src/assets/Elogo.jpg'
-//         ]);
-//       })
-//   );
-// });
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        // cache.add('/');
+        // cache.add('/index.html');
+        // cache.add('/src/js/app.js');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/js/app.js',
+          '/src/js/index.js',
+          '/src/js/subjects/field.js',
+          '/src/css/style.css',
+          '/src/assets/Elogo.jpg'
+        ]);
+      })
+  );
+});
 
 self.addEventListener('activate', function () {
   console.log('SW Activated');
