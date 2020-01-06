fdc596934b94d2befa5b105d8c05c59911e6c215
mahmoudHammad <41790090+mahmodHammad@users.noreply.github.com>
2020-01-07 00:58:37 +0200

    adding toggle fs to electronics

---
+++ src/js/toggleimage.js
@@ -0,0 +1,5 @@
+const formulaSheet = document.getElementById('fsheet')
+const fsbutton = document.querySelector('.toggle')
+fsbutton.onclick=function(){
+  formulaSheet.classList.toggle('togglesheet')
+  }
